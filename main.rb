36c11799dc7b79bf49363929a8d513264cfebd43
Denis <icebox827@gmail.com>
2022-03-27 21:27:23 -0400

    Fix linters errors

--- main.rb
+++ main.rb
@@ -9,12 +9,13 @@ class Rover
   # determine the position of the rover
   def instruction(position)
     position.each do |input|
-      if input == 'L'
+      case input
+      when 'L'
         left
         puts 'The rover turn 90 degree left'
-      elsif input == 'R'
+      when 'R'
         puts 'The rover turn 90 degree right'
-      else 
+      else
         move
         puts 'The rover move one grid ahead'
       end
@@ -24,43 +25,46 @@ class Rover
 
   # Define each position
   def left
-    if @heading == 'N'
-        puts "the rover is facing North."
+    case @heading
+    when 'N'
+      puts 'the rover is facing North.'
       @heading = 'W'
-    elsif @heading == 'W'
-        puts "the rover is facting West"
+    when 'W'
+      puts 'the rover is facting West'
       @heading = 'S'
-    elsif @heading == 'E'
-        puts "the rover is facting East"
+    when 'E'
+      puts 'the rover is facting East'
       @heading = 'N'
     else
-      puts "the rover is facing South"
+      puts 'the rover is facing South'
       @heading = 'E'
     end
   end
 
   def right
-    if @heading == 'S'
-        puts "the rover is facing West."
+    case @heading
+    when 'S'
+      puts 'the rover is facing West.'
       @heading = 'W'
-    elsif @heading == 'N'
-        puts "the rover is facting East"
+    when 'N'
+      puts 'the rover is facting East'
       @heading = 'E'
-    elsif @heading == 'W'
-        puts "the rover is facting North"
+    when 'W'
+      puts 'the rover is facting North'
       @heading = 'N'
     else
-      puts "the rover is facing South"
+      puts 'the rover is facing South'
       @heading = 'S'
     end
   end
 
   def move
-    if @heading == 'N'
+    case @heading
+    when 'N'
       @y += 1
-    elsif @heading == 'E'
+    when 'E'
       @x += 1
-    elsif @heading == 'S'
+    when 'S'
       @y -= 1
     else
       @x -= 1
@@ -68,10 +72,10 @@ class Rover
   end
 
   # Initialize each rover position and heading
-  first_rover = Rover.new(1,2,'N')
-  second_rover = Rover.new(3,3,'E')
+  first_rover = Rover.new(1, 2, 'N')
+  second_rover = Rover.new(3, 3, 'E')
 
   # Instruction for each rover th explore the plateau
-  first_rover.instruction(['L','M','L','M','L','M','L','M','M'])
-  second_rover.instruction(['M','M','R','M','M','R','M','R','R','M'])
+  first_rover.instruction(%w[L M L M L M L M M])
+  second_rover.instruction(%w[M M R M M R M R R M])
 end
